04d2d55112c0aadb4bf20bef2262534db3160495
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 16:18:28 -0700

    Add vcr for testing calls to urlockbox

--- spec/features/see_hot_links_spec.rb
+++ spec/features/see_hot_links_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe "top reads index" do
   it "shows the tops reads" do
-    create_list(:read, 5, link_id: 7)
-    create_list(:read, 2, link_id: 8)
+    VCR.use_cassette("show_top_reads") do
+      create_list(:read, 5, link_id: 7)
+      create_list(:read, 2, link_id: 8)
 
-    visit root_path
+      visit root_path
 
-    expect(page).to have_link("a great webcomic")
-    expect(page).to have_link("a time-waster")
-
+      expect(page).to have_link("a great webcomic")
+      expect(page).to have_link("a time-waster")
+    end
   end
 end
--- spec/services/link_service_spec.rb
+++ spec/services/link_service_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe LinkService do
-  it "fetches links from urlockbox" do
-    read = create(:read, link_id: 7)
+  it "fetches link from urlockbox" do
+    VCR.use_cassette("single_link_urlockbox") do
+      read = create(:read, link_id: 7)
 
-    link = LinkService.fetch_by_id(read.link_id)
+      link = LinkService.fetch_by_id(read.link_id)
 
-    expect(link.title).to eq("a great webcomic")
-    expect(link.url).to eq("http://xkcd.com")
+      expect(link.title).to eq("a great webcomic")
+      expect(link.url).to eq("http://xkcd.com")
+    end
   end
 
   it "fetches top read links from urlockbox" do
-    create_list(:read, 4, link_id: 7)
-    create_list(:read, 2, link_id: 8)
+    VCR.use_cassette("multiple_links_urlockbox") do
+      create_list(:read, 4, link_id: 7)
+      create_list(:read, 2, link_id: 8)
 
-    top_links = LinkService.fetch_top_reads
+      top_links = LinkService.fetch_top_reads
 
-    expect(top_links.first.url).to eq("http://xkcd.com")
-    expect(top_links.last.url).to eq("http://reddit.com")
+      expect(top_links.first.url).to eq("http://xkcd.com")
+      expect(top_links.last.url).to eq("http://reddit.com")
+    end
   end
 end
