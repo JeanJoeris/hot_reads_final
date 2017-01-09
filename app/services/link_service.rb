0ca6ab8ae2cb26711e930445c4c5e91a9cbc0f14
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 13:21:04 -0700

    Change to correct urlockbox url

--- app/services/link_service.rb
+++ app/services/link_service.rb
@@ -1,7 +1,7 @@
 class LinkService
   def self.fetch_by_id(link_id)
     if Rails.env == "production"
-      urlockbox_url = "https://jj-hot-reads.herokuapp.com/api/v1/links/#{link_id}"
+      urlockbox_url = "https://jj-urlockbox.herokuapp.com/api/v1/links/#{link_id}"
     else
       urlockbox_url = "http://localhost:3050/api/v1/links/#{link_id}"
     end
